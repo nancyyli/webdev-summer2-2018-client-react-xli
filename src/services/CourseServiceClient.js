b90690a2fe1ecc5932f51655c208fb0ee4142dbf
lixiang <lixiang@ccs.neu.edu>
2018-07-15 18:15:49 -0400

    Added CourseServiceClient.js

---
+++ src/services/CourseServiceClient.js
@@ -0,0 +1,56 @@
+
+
+
+class CourseService {
+    constructor() {
+
+    }
+
+    createCourse(course) {
+
+    }
+
+    deleteCourse() {
+
+    }
+
+    findAllCourses() {
+        var oneCourse = '{id: 2, title: "test", created: "yesterday", modified: "now""}';
+        return oneCourse;
+
+    }
+
+    findCourseById(id) {
+
+    }
+
+    updateCourse() {
+
+    }
+    // constructor(singletonToken) {
+    //   if (_singleton !== singletonToken)
+    //     throw new Error('Cannot instantiate directly.');
+    // }
+    // static get instance() {
+    //   if(!this[_singleton])
+    //     this[_singleton] = new CourseService(_singleton);
+    //   return this[_singleton]
+    // }
+    // findAllCourses() {
+    // //   return fetch(COURSE_API_URL)
+    // //     .then(function(response){
+    // //       return response.json();
+    // //     });
+    // }
+    // createCourse(course) {
+    //   return fetch(COURSE_API_URL, {
+    //     body: JSON.stringify(course),
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     method: 'POST'
+    //   }).then(function (response) {
+    //     return response.json();
+    //   })}
+  }
+  export default CourseService;
