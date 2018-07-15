b1523e9bca87bcb7b3f4276bcb0f42725a278c6c
lixiang <lixiang@ccs.neu.edu>
2018-07-15 16:30:44 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+ReactDOM.render(
+ <h1>Hello, world!</h1>,
+ document.getElementById('root')
+);
