d2a1417e504679e3618b8b031a724496043106c2
lixiang <lixiang@ccs.neu.edu>
2018-07-26 13:09:47 -0400

    moved Heading widget out of widget

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -2,61 +2,9 @@ import React from  'react'
 import {connect} from 'react-redux'
 import {DELETE_WIDGET} from "../constants/index"
 import * as actions from '../actions'
+import HeadingContainer from '../components/HeadingWidget'
 
-const Heading = ({widget, preview, headingTextChanged, headingSizeChanged, widgetNameChanged}) => {
-  let selectElem
-  let inputElem
-  let nameElem
-  return(
-    <div>
-      <div className="heading-widget-container" hidden={preview}>
-        {/* <h2> Heading {widget.size}</h2> */}
-        <div className="heading-widget-input-container form-group">
-        <label htmlFor="heading-widget-input">Heading Text</label>
-          <input className="form-control" id="heading-widget-input" onChange={() => headingTextChanged(widget.id, inputElem.value)}
-                 value={widget.text}
-                 ref={node => inputElem = node} placeholder="Enter Heading Text"/>
-        </div>
-        <div className="heading-widget-size-container form-group">
-        <label htmlFor="heading-widget-size">Heading Size</label>
-          <select className="form-control" id="heading-widget-size" onChange={() => headingSizeChanged(widget.id, selectElem.value)}
-                  value={widget.size}
-                  ref={node => selectElem = node}>
-            <option value="1">Heading 1</option>
-            <option value="2">Heading 2</option>
-            <option value="3">Heading 3</option>
-          </select>
-        </div>
-        <div className="heading-widget-name-container form-group">
-          <label htmlFor="heading-widget-name"> Widget Name </label>
-            <input className="form-control" id="heading-widget-name" onChange={() => widgetNameChanged(widget.id, nameElem.value)} value={widget.name} ref={node => nameElem = node} placeholder="Enter Widget Name"/>
-        </div>
-          <h3>Preview</h3>
-
-      </div>
-      <div className="preview-heading">
-        {widget.size == 1 && <h1>{widget.text}</h1>}
-        {widget.size == 2 && <h2>{widget.text}</h2>}
-        {widget.size == 3 && <h3>{widget.text}</h3>}
-      </div>
-    </div>
-  )
-}
-const dispatchToPropsMapper = dispatch => ({
-  headingTextChanged: (widgetId, newText) =>
-    actions.headingTextChanged(dispatch, widgetId, newText),
-  headingSizeChanged: (widgetId, newSize) =>
-    actions.headingSizeChanged(dispatch, widgetId, newSize),
-  widgetNameChanged: (widgetId, newName) =>
-    actions.widgetNameChanged(dispatch, widgetId, newName)
-})
-
-const stateToPropsMapper = state => ({
-  preview: state.preview
-})
-const HeadingContainer = connect(stateToPropsMapper, dispatchToPropsMapper)(Heading)
-
-const Widget = ({widget, preview, dispatch}) => {
+const Widget = ({widget, preview, dispatch, lessonId, deleteWidget}) => {
   let selectElement
   return(
     <li className="widget-item" >
@@ -69,13 +17,10 @@ const Widget = ({widget, preview, dispatch}) => {
             id: widget.id,
             widgetType: selectElement.value
           })} ref={node => selectElement = node}>
-           //not convinced this works
         <option>Heading</option>
       </select>
 
-      <button className="btn btn-danger delete-widget-btn" onClick={e => (
-        dispatch({type: DELETE_WIDGET, id: widget.id})
-      )}>
+      <button className="btn btn-danger delete-widget-btn" onClick={() => deleteWidget(widget.id, lessonId)}>
           <i className="fa fa-trash delete-module"></i>
         </button>
       </div>
@@ -86,8 +31,16 @@ const Widget = ({widget, preview, dispatch}) => {
   )
 }
 
-const WidgetContainer = connect(state => ({
-  preview: state.preview
-}))(Widget)
+const dispatchToPropsMapper = dispatch => ({
+    deleteWidget: (widgetId, lessonId) =>
+        actions.deleteWidget(dispatch, widgetId, lessonId)  
+  })
+  
+const stateToPropsMapper = state => ({
+    lessonId: state.lessonId,
+    preview: state.preview
+})
+
+const WidgetContainer = connect(stateToPropsMapper, dispatchToPropsMapper)(Widget)
 
 export default WidgetContainer
